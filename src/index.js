589d834dc9ed76730d25b87d7118ded7af4a3a43
Rootdiv <rootdiv@mail.ru>
2022-07-02 11:50:05 +0300

    feat: styling header, catalog, footer and add Swiper slider

--- src/index.js
+++ src/index.js
@@ -2,3 +2,38 @@ import './index.html';
 import './cart.html';
 import './card.html';
 import './index.scss';
+
+import Swiper, { Thumbs, Scrollbar, Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/scrollbar';
+import 'swiper/css/pagination';
+
+const thumbSwiper = new Swiper('.card__slide-thumb', {
+  spaceBetween: 44,
+  slidesPerView: 3,
+  modules: [Scrollbar],
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+});
+
+new Swiper('.card__image', {
+  spaceBetween: 10,
+  slidesPerView: 1,
+  modules: [Thumbs],
+  thumbs: {
+    swiper: thumbSwiper,
+    slideThumbActiveClass: 'card__thumb-btn_active',
+  },
+});
+
+new Swiper('.recommended__carousel', {
+  modules: [Pagination],
+  slidesPerView: 5,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
