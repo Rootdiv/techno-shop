1d6c389768c69ca0d0902989522970eb8f9a8a52
Rootdiv <rootdiv@mail.ru>
2022-07-07 00:05:15 +0300

    feat: adaptive CSS styles for the catalog

--- src/index.js
+++ src/index.js
@@ -13,8 +13,31 @@ const paginationWrapper = document.querySelector('.pagination');
 const pageURL = new URL(location);
 const page = +pageURL.searchParams.get('page') || 1;
 
+let isMobile = false;
+
+const startPagination = () => {
+  if (window.innerWidth <= 560) {
+    pagination(paginationWrapper, 20, page, 4);
+    isMobile = true;
+  } else {
+    pagination(paginationWrapper, 20, page, 6);
+    isMobile = false;
+  }
+};
+
 try {
-  pagination(paginationWrapper, 20, page, 6);
+  startPagination();
+  window.addEventListener('resize', () => {
+    if (window.innerWidth <= 560 && !isMobile) {
+      pagination(paginationWrapper, 20, page, 4);
+      isMobile = true;
+    }
+
+    if (window.innerWidth > 560 && isMobile) {
+      pagination(paginationWrapper, 20, page, 6);
+      isMobile = false;
+    }
+  });
 } catch (error) {
   console.warn(error);
   console.warn('Это не главная страница');
