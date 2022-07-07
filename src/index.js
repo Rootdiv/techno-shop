c605fc9a005c4fd306314dca69eb9fea179d52b1
Rootdiv <rootdiv@mail.ru>
2022-07-08 00:40:29 +0300

    feat: getting one product and recommended

--- src/index.js
+++ src/index.js
@@ -3,56 +3,73 @@ import './cart.html';
 import './card.html';
 import './index.scss';
 
-import Swiper, { Thumbs, Scrollbar, Pagination } from 'swiper';
+import Swiper, { Pagination } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/scrollbar';
 import 'swiper/css/pagination';
+
 import { startPagination } from './modules/pagination';
-import { getGoods } from './modules/goodsService';
+import { getGoods, getGoodsItem } from './modules/goodsService';
 import { renderGoods } from './modules/renderGoods';
 import preloader from './img/preloader.svg';
+import { renderItem } from './modules/renderItem';
+import { renderRecommended } from './modules/renderRecommended';
 
 try {
-  const paginationWrapper = document.querySelector('.pagination');
   const goodsList = document.querySelector('.goods__list');
-  const pageURL = new URL(location);
-  const page = +pageURL.searchParams.get('page') || 1;
+  if (goodsList) {
+    const paginationWrapper = document.querySelector('.pagination');
+    const pageURL = new URL(location);
+    const page = +pageURL.searchParams.get('page') || 1;
 
-  goodsList.insertAdjacentHTML(
-    'afterbegin',
-    `<div class="goods__preloader">
-      <img src="${preloader}" alt="Загрузка..." />
-    </div>`,
-  );
+    goodsList.insertAdjacentHTML(
+      'afterbegin',
+      `<div class="goods__preloader">
+        <img src="${preloader}" alt="Загрузка..." />
+      </div>`,
+    );
 
-  getGoods({ page }).then(({ goods, pages, page }) => {
-    renderGoods(goodsList, goods);
-    startPagination(paginationWrapper, pages, page);
-  });
+    getGoods({ page }).then(({ goods, pages, page }) => {
+      renderGoods(goodsList, goods);
+      startPagination(paginationWrapper, pages, page);
+    });
+  }
 } catch (error) {
   console.warn(error);
-  console.warn('Это не главная страница');
 }
 
-const thumbSwiper = new Swiper('.card__slide-thumb', {
-  spaceBetween: 44,
-  slidesPerView: 3,
-  modules: [Scrollbar],
-  scrollbar: {
-    el: '.swiper-scrollbar',
-    draggable: true,
-  },
-});
+try {
+  const card = document.querySelector('.card');
+  const recommendedCarousel = document.querySelector('.recommended__carousel');
+  if (card) {
+    const pageURL = new URL(location);
+    const id = pageURL.searchParams.get('id');
 
-new Swiper('.card__image', {
-  spaceBetween: 10,
-  slidesPerView: 1,
-  modules: [Thumbs],
-  thumbs: {
-    swiper: thumbSwiper,
-    slideThumbActiveClass: 'card__thumb-btn_active',
-  },
-});
+    const preload = document.createElement('div');
+    preload.className = 'card__preloader';
+    preload.insertAdjacentHTML(
+      'afterbegin',
+      `<div class="goods__preloader">
+        <img src="${preloader}" alt="Загрузка..." />
+      </div>`,
+    );
+    card.append(preload);
+
+    getGoodsItem(id)
+      .then(item => {
+        renderItem(item);
+        preload.remove();
+        return item.category;
+      })
+      .then(category => getGoods({ category }))
+      .then(data => {
+        const goods = data.goods.filter(item => item.id !== id);
+        renderRecommended(recommendedCarousel, goods);
+      });
+  }
+} catch (error) {
+  console.warn(error);
+}
 
 new Swiper('.recommended__carousel', {
   modules: [Pagination],
---
+++ src/modules/renderItem.js
@@ -0,0 +1,108 @@
+import { API_URI } from './var';
+import Swiper, { Thumbs, Scrollbar } from 'swiper';
+
+const createImageSlider = largeImages => {
+  const ul = document.createElement('ul');
+  ul.className = 'swiper-wrapper';
+  const cardImageSlides = largeImages.map(url => {
+    const li = document.querySelector('li');
+    li.className = 'swiper-slide';
+    const img = new Image();
+    img.src = `${API_URI}/${url}`;
+    li.append(img);
+    return li;
+  });
+
+  ul.append(...cardImageSlides);
+  return ul;
+};
+
+const createImageThumbSlider = smallImages => {
+  const ul = document.createElement('ul');
+  ul.className = 'swiper-wrapper';
+  const cardImageSlides = smallImages.map(url => {
+    const li = document.querySelector('li');
+    li.className = 'swiper-slide';
+    const button = document.createElement('button');
+    button.className = 'card__thumb-btn';
+    const img = new Image();
+    img.src = `${API_URI}/${url}`;
+    button.append(img);
+    li.append(button);
+    return li;
+  });
+
+  ul.append(...cardImageSlides);
+  return ul;
+};
+
+const createParams = params => {
+  const list = [];
+  for (const key in params) {
+    const li = document.createElement('li');
+    li.className = 'card__params-item';
+    li.insertAdjacentHTML('afterbegin', `<span>${key}:</span><span>${params[key]}</span>`);
+    list.push(li);
+  }
+  return list;
+};
+
+const createDescription = descriptions => {
+  const list = [];
+  for (const description of descriptions) {
+    const p = document.createElement('p');
+    p.textContent = description;
+
+    list.push(p);
+  }
+  return list;
+};
+
+export const renderItem = item => {
+  const cardImage = document.querySelector('.card__image');
+  cardImage.append(createImageSlider(item.images.large));
+
+  const cardSliderThumb = document.querySelector('.card__slider-thumb');
+  const swiperScrollbar = document.createElement('div');
+  swiperScrollbar.className = 'swiper-scrollbar';
+  cardSliderThumb.append(createImageThumbSlider(item.images.small), swiperScrollbar);
+
+  const cardTitle = document.querySelector('.card__title');
+  cardTitle.textContent = item.title;
+  const cardVendorCode = document.querySelector('.card__vendor-code');
+  cardVendorCode.textContent = `Артикул: ${item.id}`;
+  const cardPrice = document.querySelector('.card__price');
+  cardPrice.textContent = new Intl.NumberFormat('ru-RU', {
+    style: 'currency',
+    currency: 'RUB',
+    maximumFractionDigits: 0,
+  }).format(item.price);
+  const cardAddCart = document.querySelector('.card__add-cart');
+  cardAddCart.dataset.idGoods = item.id;
+
+  const cardParamsList = document.querySelector('.card__params-list');
+  cardParamsList.append(...createParams(item.characteristic));
+
+  const cardDescriptionText = document.querySelector('.card__description-text');
+  cardDescriptionText.append(...createDescription(item.description));
+
+  const thumbSwiper = new Swiper(cardSliderThumb, {
+    spaceBetween: 44,
+    slidesPerView: 3,
+    modules: [Scrollbar],
+    scrollbar: {
+      el: swiperScrollbar,
+      draggable: true,
+    },
+  });
+
+  new Swiper(cardImage, {
+    spaceBetween: 10,
+    slidesPerView: 1,
+    modules: [Thumbs],
+    thumbs: {
+      swiper: thumbSwiper,
+      slideThumbActiveClass: 'card__thumb-btn_active',
+    },
+  });
+};
