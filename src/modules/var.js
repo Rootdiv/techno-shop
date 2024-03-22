ddbafb35b6befc5dc73a556ea1c44e479445b388
Rootdiv <rootdiv9152@gmail.com>
2024-03-22 16:42:04 +0300

    feat: changed api server

--- src/modules/var.js
+++ src/modules/var.js
@@ -1,1 +1,2 @@
-export const API_URI = `${location.protocol}//${location.hostname}:3024`;
+export const API_URI =
+  process.env.NODE_ENV === 'production' ? 'https://api.rootdiv.ru/ts' : 'http://localhost:3024';
